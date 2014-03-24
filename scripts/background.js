61dfd85c396278cdc27706c70f6a15d3d180efd8
rauljuarez <rauljuarez@gmail.com>
2014-03-24 08:58:54 -0300

    All files

---
+++ scripts/background.js
@@ -0,0 +1,97 @@
+﻿//You sholud put here your URL Shortener Service, was used for TopicFlower Service, but if yours returns a JSON with the URL, go for it
+var url = 'THISISTHEPLACETOPUTYOURURLSHORTENER';
+
+var timer = null;
+var milliseconds = 20000;
+
+function chromeExOAuthOnAuthorize(token, secret) {
+    if (typeof OnAuthorizeCallBack === 'function')
+        OnAuthorizeCallBack();
+}
+
+function copyToClipboard(text) {
+    var input = document.getElementById('url');
+
+    if (input == undefined)
+        return;
+
+    input.value = text;
+    input.select();
+
+    document.execCommand('copy', false, null);
+}
+
+function shortenUrl(longUrl, incognito, callback)
+{	
+	var response;
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('POST', url, true);
+	xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	
+	var auth = {};
+	
+	xmlhttp.onreadystatechange = function()
+	{
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) 
+		{
+			clearTimeout(timer);
+
+			response = JSON.parse(xmlhttp.responseText);
+			if(response.shortener == undefined)
+			{
+				callback({status: 'error', message: response.error.message});
+			}
+			else	
+			{
+				callback({status: 'success', message: response.shortener.url});
+			}
+			
+		}
+	}
+
+	xmlhttp.send('url='+longUrl);
+	timer = setTimeout(function()
+	{
+		xmlhttp.abort();
+		callback({status: 'error', message: chrome.i18n.getMessage('timeout_occurred')});
+	}
+	, milliseconds);
+}
+
+function onMessage(message, sender, callback) {
+    switch (message.type) {
+    case 'shortcut':
+        chrome.tabs.getSelected(null, function (tab) {
+            shortenUrl(tab.url, tab.incognito, function (response) {
+                if (response.status != 'error') {
+                    if (message.shortcut == 'copy') {
+                        copyToClipboard(response.message);
+                    } else {
+                        tab.shortenedUrl = response.message;
+                        share(message.shortcut, tab);
+                    }
+                }
+
+                callback(response);
+            });
+        });
+        break;
+
+    case 'preferences':
+        var shortcuts = shortcutsGetter();
+        var preferences = preferencesGetter();
+        shortcuts.shortcuts_enabled = preferences.shortcuts_enabled;
+
+        callback(shortcuts);
+        break;
+    }
+}
+
+function init() {
+    createContextMenu();
+    chrome.extension.onMessage.addListener(onMessage);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    init();
+});
---
+++ scripts/popup.js
@@ -0,0 +1,139 @@
+﻿var background_page = chrome.extension.getBackgroundPage();
+var current_tab;
+
+function serviceClick(service)
+{    
+	return function () 
+	{
+       share(service, current_tab);
+    }
+}
+
+function addActions()
+{
+	var show_actions = false;
+	
+	var actions = actionsGetter();
+	var action_element = document.getElementById('actions');
+	
+
+	for (var action in actions)
+	{		
+		if(!actions[action])
+			continue;
+
+		show_actions = true;
+			
+		var element = document.createElement('div');
+		element.setAttribute('class', 'item');
+			
+		var image = document.createElement('img');
+		image.setAttribute('width', '16');
+
+		var nbsp = document.createTextNode('\u00A0');
+		
+		var anchor = document.createElement('a');
+		anchor.setAttribute('href', '#');
+		
+		switch(action)
+		{
+			case 'copy':
+				image.id = 'copy_link';
+				image.setAttribute('src', 'images/copy.png');
+				
+				anchor.innerText = chrome.i18n.getMessage('copy');
+				anchor.addEventListener('click', function () 
+												{ 
+													background_page.copyToClipboard(current_tab.shortenedUrl); 
+													window.close(); 
+												}
+												, false);
+			break;
+		}
+		
+		element.appendChild(image);
+		element.appendChild(nbsp);
+		element.appendChild(anchor);
+		action_element.appendChild(element);
+	}
+	
+	if(show_actions)
+		document.getElementById('actions').setAttribute('style', 'visibility:visible; display:block;');
+}
+
+function addServices()
+{		
+	var show_services = false;
+
+	var services = servicesGetter();
+	var services_element = document.getElementById('services');
+	
+	for (var service in services)
+	{		
+		if(!services[service] || servicesJSON[service] == undefined)
+			continue;
+			
+		show_services = true;
+	
+		var element = document.createElement('div');
+		element.setAttribute('class', 'item');
+		
+		var image = document.createElement('img');
+		image.setAttribute('width', '16');
+		image.setAttribute('src', 'images/' + servicesJSON[service].icon);
+
+		var nbsp = document.createTextNode('\u00A0');
+		
+		var anchor = document.createElement('a');
+		anchor.setAttribute('href', '#');
+		anchor.innerText = servicesJSON[service].name;
+		anchor.addEventListener('click', serviceClick(service));
+		
+		//element.appendChild(image);
+		//element.appendChild(nbsp);
+		//element.appendChild(anchor);
+		//services_element.appendChild(element);
+	}	
+	
+	
+}
+
+function onResponse(response)
+{			
+	if(response.status == 'error')
+	{
+		document.getElementById('loading').innerText = response.message;
+	}
+	else
+	{
+		var preferences = preferencesGetter();
+	
+		current_tab.shortenedUrl = response.message;
+	
+		if(preferences.auto_copy)
+			background_page.copyToClipboard(current_tab.shortenedUrl);
+	
+		document.getElementById('url').innerText = current_tab.shortenedUrl;
+		
+		
+		addActions();
+		
+		document.getElementById('loading').setAttribute('style', 'visibility:hidden; display:none;');
+		document.getElementById('response').setAttribute('style', 'visibility:visible; display:block;');
+	}
+}
+
+function init()
+{
+	chrome.tabs.getSelected(null, function(tab) 
+	{
+		current_tab = tab;
+		background_page.shortenUrl(tab.url, tab.incognito, onResponse);
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function () 
+{
+	document.getElementById('loading').innerText = chrome.i18n.getMessage('shortening');
+	init();
+});
